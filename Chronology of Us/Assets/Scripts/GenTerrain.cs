420fdacdd5179b18002031cdf4a059e1e2a6bca6
Vulpire <nbw201@gmail.com>
2018-11-13 20:39:32 -0500

    Base Project

---
+++ Chronology of Us/Assets/Scripts/GenTerrain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenTerrain : MonoBehaviour {
+
+    public GameObject DirtTile;
+    public GameObject GrassTile;
+    public GameObject StoneTile;
+
+    public int ChunkW;
+    public float heightmultiplyer;
+    public int heightadd;
+
+    public float smoothness;
+
+    float seed;
+
+	void Start () {
+        Generate();
+        seed = Random.Range(-10000f, 10000f);
+	}
+	
+	public void Generate () {
+        for (int i = 0; i < ChunkW; i++)
+        {
+            int h = Mathf.RoundToInt(Mathf.PerlinNoise(seed, i / smoothness) * heightmultiplyer) + heightadd;
+        for (int j = 0; j < h; j++)
+        {
+        GameObject selectedTile;
+                if (j < h -4)
+                {
+                    selectedTile = StoneTile;                   
+                }
+                else if (j < h - 1)
+                {
+                    selectedTile = DirtTile;
+                }
+                else
+                {
+                    selectedTile = GrassTile;
+                }
+                Instantiate(selectedTile, new Vector3(i, j), Quaternion.identity);
+            }
+        }
+	}
+}
